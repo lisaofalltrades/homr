492578804062a1fc7ef37edd0cd508a9544d60a1
the crafty coder <lisathuanguyen@gmail.com>
2020-07-03 15:57:28 -0700

    render charts

--- client-react/src/Dashboard.js
+++ client-react/src/Dashboard.js
@@ -1,5 +1,30 @@
 import React from 'react'
+import { Input, Button, Header, Tab, Dropdown } from 'semantic-ui-react'
+import Map from './Map'
+import Metrics from './Metrics'
 
-export default function Dashboard () {
-  return <h1>Dashboard</h1>
+export default function Dashboard (props) {
+  const panes = [
+    {
+      menuItem: 'Dashboard',
+      render: () =>
+        <Tab.Pane attached={false}>
+          <div style={{ 'text-align': 'center' }}>
+            <Metrics />
+          </div>
+        </Tab.Pane>
+    },
+    {
+      menuItem: 'Map',
+      render: () =>
+        <Tab.Pane attached={false}>
+          <div style={{ 'text-align': 'center' }}>
+            <Map />
+          </div>
+        </Tab.Pane>
+    }
+  ]
+  return (
+    <Tab panes={panes} menu={{ pointing: true }} style={{ width: '300px', margin: '0 auto' }} />
+  )
 }
