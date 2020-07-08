844c497846d3f2bf47f081835156fcf9762de93d
the crafty coder <lisathuanguyen@gmail.com>
2020-07-07 21:38:45 -0700

    notes model

---
+++ client-react/models/Note.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+const { ObjectId } = Schema.Types
+
+const noteSchema = Schema({
+  date: {
+    type: String,
+    required: false
+  },
+  category: {
+    type: String,
+    required: false
+  },
+  address: {
+    type: String,
+    required: false
+  },
+  description: {
+    type: String,
+    required: false
+  },
+  patient: {
+    type: ObjectId,
+    required: true,
+    ref: 'Patient'
+  },
+  author: {
+    type: ObjectId,
+    required: true,
+    ref: 'User'
+  }
+})
+
+noteSchema.statics.register = async function (date, category, address, description, patient, author) {
+  const note = new this()
+  note.date = date
+  note.category = category
+  note.address = address
+  note.description = description
+  note.patient = patient
+  note.author = author
+
+  await note.save()
+
+  return note
+}
+
+const note = mongoose.model('note', noteSchema)
+
+module.exports = note
