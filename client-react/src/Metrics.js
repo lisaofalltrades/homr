9a61ad2bd4db66d5889105f59f1b29817ca52c5b
the crafty coder <lisathuanguyen@gmail.com>
2020-07-14 11:28:03 -0700

    pass illneschart token

--- client-react/src/Metrics.js
+++ client-react/src/Metrics.js
@@ -2,12 +2,21 @@ import React from 'react'
 import IncidentsByDistrict from './graphs/incidentsByDistrict'
 import IllnessChart from './graphs/illnessChart'
 
-export default function Metrics () {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <IncidentsByDistrict />
-      <IllnessChart />
-    </div>
-  )
+export default class Metrics extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      token: this.props.token
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        <IncidentsByDistrict />
+        <IllnessChart token={this.state.token} />
+      </div>
+    )
+  }
 }
