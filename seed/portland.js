309af9523acd270e7f5fcf27dfe4498d6216c798
the crafty coder <lisathuanguyen@gmail.com>
2020-07-15 11:30:15 -0700

    moved portland addresses to another file/folder

---
+++ seed/portland.js
@@ -0,0 +1,47 @@
+// Portland Addresses
+const addresses = [
+  [
+    '3504 SE 92nd Ave, Portland, OR 97266',
+    { lat: 45.496590, long: -122.566570 }
+  ],
+  [
+    '16211 SE Division St, Portland, OR 97236',
+    { lat: 45.504930, long: -122.496120 }
+  ],
+  [
+    '3432 SE 25th Ave, Portland, OR 97202',
+    { lat: 45.498470, long: -122.640320 }
+  ],
+  [
+    '4229 NE 122nd Ave, Portland, OR 97230',
+    { lat: 45.552910, long: -122.537260 }
+  ],
+  [
+    '2975 NE Hogan Dr, Gresham, OR 97030',
+    { lat: 45.518390, long: -122.412950 }
+  ],
+  [
+    '1135 NE Martin Luther King Jr Blvd, Portland, OR 97232',
+    { lat: 45.5311136, long: -122.6620034 }
+  ],
+  [
+    '1122 SE Hawthorne Blvd, Portland, OR 97214',
+    { lat: 45.5120579, long: -122.6539873 }
+  ],
+  [
+    '8218 NE Glisan St, Portland, OR 97220',
+    { lat: 45.518390, long: -122.412950 }
+  ],
+  [
+    '4200 SE 82nd Ave, Portland, OR 97266',
+    { lat: 45.5261283, long: -122.5786043 }
+  ],
+  [
+    '3527 SE 122nd Ave, Portland, OR 97236',
+    { lat: 45.4962466, long: -122.5391627 }
+  ]
+]
+
+module.exports = {
+  addresses: addresses
+}
