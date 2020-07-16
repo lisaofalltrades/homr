40aa23efa57bede8df2eb7799fdc48b84bebed59
the crafty coder <lisathuanguyen@gmail.com>
2020-07-15 21:23:48 -0700

    remove secrets.pu

--- client-react/src/Map.js
+++ client-react/src/Map.js
@@ -7,7 +7,7 @@ import {
 } from '@react-google-maps/api'
 import { formatRelative } from 'date-fns'
 import mapStyles from './mapstyles'
-import secret from './secrets'
+// import secret from './secrets'
 
 // globals
 const mapContainerStyle = {
@@ -64,9 +64,8 @@ export default function Map (props) {
   console.log(cords)
 
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || secret.key
-    // googleMapsApiKey: secret.key
-    // googleMapsApiKey: ''
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    // googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || secret.key
   })
 
   // this is the state of this component
