3a05ffcbe7da33f5a5c32f39d0dca9dbafabd6c6
the crafty coder <lisathuanguyen@gmail.com>
2020-07-15 21:19:26 -0700

    save

--- faker.js
+++ faker.js
@@ -1,2 +1,9 @@
+// mongo_uri
+'mongodb://heroku_906kqvg2:8t1anstopporkf4orjml2qviqo@ds155418.mlab.com:55418/heroku_906kqvg2'
 
+'mongodb://user:pass@host:port/db'
+
+mongo host: port/db -u user -p pass yourSeedFile.js
+
+'mongo ds155418.mlab.com:55418/heroku_906kqvg2 -u user -p pass yourSeedFile.js'
 
